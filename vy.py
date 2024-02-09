6f9d3c4f392f0093d8309539cc49f122921c3c82
huynguyen2703 <huynguyenquoc2703@gmail.com>
2024-02-09 01:35:10 -0800

    Change file to MP3

--- vy.py
+++ vy.py
@@ -49,7 +49,7 @@ def gallery_page():
     path = "/Vy-Gallery.com"
     sound_file = os.environ.get("SOUND_PATH")
     if request.path == path:
-        threading.Thread(target=playsound, args=(sound_file,), daemon=True).start()
+        play_sound(sound_file)
     return render_template("homepage.html")
 
 
